e6272d4fbcb321373e1880632c23e91ebdfa9805
PrasanthJayaraman <prasanthj@outlook.com>
2018-12-03 22:44:36 +0530

    Fixed 401 error

--- src/app/services/httpservice.service.ts
+++ src/app/services/httpservice.service.ts
@@ -1,7 +1,13 @@
+import { Router } from '@angular/router';
 import { AlertService } from './alert.service';
 import { StoreService } from './../store/store.service';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
@@ -9,20 +15,20 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class Httpservice {
   constructor(
-    private http: HttpClient,
-    private store: StoreService,
-    private alert: AlertService
+    public http: HttpClient,
+    public store: StoreService,
+    public alert: AlertService,
+    public router: Router
   ) {}
   MASTER_URL = environment.masterURL;
   BASE_URL = environment.baseUrl;
   self = this;
 
   public GET(API_URL): Observable<any> {
-    //httpOptions.set('Authorization', );
     return this.http
       .get(`${this.BASE_URL}${API_URL}`, { headers: this.getHeaders() })
       .map(response => {
@@ -32,27 +38,26 @@ export class Httpservice {
   }
 
   public POST(API_URL, data = {}): Observable<any> {
-    //httpOptions.set('Authorization', );
     return this.http
-      .post(`${this.BASE_URL}${API_URL}`, data, { headers: this.getHeaders() })
+      .post(`${this.BASE_URL}${API_URL}`, data, {
+        headers: this.getHeaders()
+      })
       .map(response => {
         return response;
       })
-      .catch(this.handleError);
+      .catch((err, caught) => this.handleError(err, caught));
   }
 
   public DELETE(API_URL): Observable<any> {
-    //httpOptions.set('Authorization', );
     return this.http
       .delete(`${this.BASE_URL}${API_URL}`, { headers: this.getHeaders() })
       .map(response => {
         return response;
       })
-      .catch(this.handleError);
+      .catch((err, caught) => this.handleError(err, caught));
   }
 
   public LOGIN(API_URL, data = {}): Observable<any> {
-    //httpOptions.set('Authorization', );
     return this.http
       .post(`${this.MASTER_URL}${API_URL}`, data, {
         headers: this.getHeaders()
@@ -60,22 +65,25 @@ export class Httpservice {
       .map(response => {
         return response;
       })
-      .catch(this.handleError);
+      .catch((err, caught) => this.handleError(err, caught));
   }
 
-  private handleError(error: Response | any) {
-    this.self.alert.showLoader(false);
-    this.self.alert.showAlert("Server Error", "error");
-    console.error("ApiService::handleError", error);
+  private handleError(error: Response | any, caught) {
+    if (error.status === 401) {
+      this.store.clear();
+      this.router.navigate(['login']);
+    }
+    this.alert.showLoader(false);
+    this.alert.showAlert('Server is Busy! Please try again.', 'danger');
     return Observable.throw(error);
   }
 
   private getHeaders() {
     let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append("Content-Type", "application/json");
-    this.store.get("token")
-      ? (headers = headers.append("Authorization", this.store.get("token")))
-      : "";
+    headers = headers.append('Content-Type', 'application/json');
+    this.store.get('token')
+      ? (headers = headers.append('Authorization', this.store.get('token')))
+      : '';
     return headers;
   }
 }
