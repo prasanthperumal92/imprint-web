99842567edd57be302ba1ae0b295fd684ecea822
PrasanthJayaraman <prasanthj@outlook.com>
2018-12-16 00:50:59 +0530

    Added Task Listing + View Task + share Task

--- src/app/helpers/header/header.component.ts
+++ src/app/helpers/header/header.component.ts
@@ -4,28 +4,33 @@ import { Router } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
+  public name: string;
   @Input() title: string;
-  constructor(public router: Router, public store: StoreService, public alert: AlertService) {
+  constructor(
+    public router: Router,
+    public store: StoreService,
+    public alert: AlertService
+  ) {
+    let tmp = this.store.get('profile');
+    this.name = tmp.employee.name;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   goto(path: string) {
     this.router.navigate([path]);
   }
 
   logout() {
-    this.store.clear('isLoggedIn');
+    this.store.clear("isLoggedIn");
     setTimeout(() => {
-      this.goto('login');
-      this.alert.showAlert('Logged Out Successfully', 'success');
+      this.goto("login");
+      this.alert.showAlert("Logged Out Successfully", "success");
     }, 1000);
   }
 }
