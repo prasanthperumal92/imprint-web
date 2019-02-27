cefe91a7a0967918e24b8880efdfe3845d9b810f
PrasanthJayaraman <prasanthj@outlook.com>
2019-02-28 00:34:56 +0530

    Fixed 400 error messages from the APIs

--- src/app/services/httpservice.service.ts
+++ src/app/services/httpservice.service.ts
@@ -1,21 +1,21 @@
-import { Router } from '@angular/router';
-import { AlertService } from './alert.service';
-import { StoreService } from './../store/store.service';
-import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { AlertService } from "./alert.service";
+import { StoreService } from "./../store/store.service";
+import { Injectable } from "@angular/core";
 import {
   HttpClient,
   HttpHeaders,
   HttpEvent,
   HttpInterceptor
-} from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+} from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class Httpservice {
   constructor(
@@ -82,10 +82,12 @@ export class Httpservice {
   private handleError(inst, error: Response | any, caught) {
     if (error.status === 401) {
       inst.store.clear();
-      inst.router.navigate(['login']);
-      inst.alert.showAlert('Session Timeout,! Please Login again.', 'danger');
+      inst.router.navigate(["login"]);
+      inst.alert.showAlert("Session Timeout,! Please Login again.", "danger");
+    } else if (error.status === 400) {
+      inst.alert.showAlert(error.error.message || "Server is Busy! Please try again.", "warning");
     } else {
-      inst.alert.showAlert('Server is Busy! Please try again.', 'danger');
+      inst.alert.showAlert("Server is Busy! Please try again.", "danger");
     }
     inst.alert.showLoader(false);
 
@@ -94,10 +96,10 @@ export class Httpservice {
 
   private getHeaders() {
     let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append('Content-Type', 'application/json');
-    this.store.get('token')
-      ? (headers = headers.append('Authorization', this.store.get('token')))
-      : '';
+    headers = headers.append("Content-Type", "application/json");
+    this.store.get("token")
+      ? (headers = headers.append("Authorization", this.store.get("token")))
+      : "";
     return headers;
   }
 }
